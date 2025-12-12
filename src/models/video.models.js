98dd9977e4f4ba58cc31dc12154e17235c1def93
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-13 00:01:15 +0530

    feat: add aggregate pagination plugin to Video schema

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new mongoose.Schema(
     {
         videoFile : {
@@ -38,6 +38,6 @@ const videoSchema = new mongoose.Schema(
     }, { timestamps: true });
 
 
-
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
