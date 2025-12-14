0c48d638fb83163799305d00c8389a2018ae2334
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-14 19:36:57 +0530

    current-user routes configured

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword} from '../controllers/user.controller.js'
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser} from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,6 +25,7 @@ router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
 router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+router.route('/current-user').post(verifyJWT, getCurrentUser)
 
 
 export default router;
