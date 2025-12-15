91502f1cbfe9959a61acf7df1a92d3039b90f797
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-15 21:24:57 +0530

    routes bug fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,8 @@ import {
     updateUserAvatar, 
     updateUserCoverImage,
     getUserChannelProfile,
-    getWatchHistory
+    getWatchHistory,
+    updateAccountDetails
 } from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -39,7 +40,7 @@ router.route('/change-password').post(verifyJWT, changeCurrentPassword)
 router.route('/current-user').get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route('/update-avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
-router.route('/cover-image"').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route('/cover-image').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 
 router.route('/c/:username').get(verifyJWT, getUserChannelProfile)
