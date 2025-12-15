ad7d3752446c9757ba77f8a2a2c5abc1e07ff7e4
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-15 20:20:56 +0530

    get user channel profile and get watch history configured

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage} from '../controllers/user.controller.js'
+import {
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -30,5 +41,8 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route('/update-avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
 router.route('/cover-image"').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
+
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile)
+router.route('/watch-history').get(verifyJWT, getWatchHistory)
 
 export default router;
