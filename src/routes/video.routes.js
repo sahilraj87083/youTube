52bdc40443d6a5d3ada68a08213e645e8a3d2219
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-20 11:40:31 +0530

    get all video and publish a video routes configured

---
+++ src/routes/video.routes.js
@@ -0,0 +1,31 @@
+import {
+    getAllVideos,
+    publishAVideo,
+} from '../controllers/video.controller.js'
+
+import { Router } from 'express'
+import { upload } from '../middlewares/multer.middleware.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
+
+const router = Router();
+
+router
+.route('/')
+.get(getAllVideos)
+.post(
+    verifyJWT,
+    upload.fields = ([
+        {
+            name: "videoFile",
+            maxCount: 1
+        },
+        {
+            name: "thumbnail",
+            maxCount: 1
+        }
+    ]),
+    publishAVideo
+)
+
+
+export default router;
