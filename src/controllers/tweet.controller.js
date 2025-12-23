b28bef37ec123bfb0bac3ba5dc3b76e62c8d9b0b
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-23 20:22:51 +0530

    feat(tweet): add update tweet functionality with ownership check

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -35,6 +35,54 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {content} = req.body
+    const {tweetId} = req.params
+
+    if(!content || typeof content !== 'string' || !content.trim()){
+        throw new ApiError(400, 'content is required')
+    }
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweetId")
+    }
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet){
+        throw new ApiError(404, 'Tweet not found')
+    }
+
+    if(tweet.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403, "You are not authorized to update this tweet");
+    }
+
+    if (tweet.content === content.trim()) {
+        return res.status(200).json(
+            new ApiResponse(200, tweet, "No changes detected")
+        );
+    }
+
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set : {
+                content : content
+            }
+        },
+        {
+            new : true
+        }
+    )
+
+    if(!updatedTweet){
+        throw new ApiError(500, "Failed to edit tweet please try again");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"));
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
