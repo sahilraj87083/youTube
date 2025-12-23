1a42d7be0c8c1475201e304e4c7b974a62fc9c88
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-23 20:42:41 +0530

    feat(tweet): add get user tweets with likes and owner details

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -32,6 +32,81 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid userId")
+    }
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match : {
+                owner : new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $sort : {
+                createdAt : -1
+            }
+        },
+        {
+            $lookup : {
+                from : 'users',
+                localField : 'owner',
+                foreignField : '_id',
+                as : 'ownerDetails',
+                pipeline : [
+                    {
+                        $project : {
+                            username : 1,
+                            "avatar.url": 1,
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup : {
+                from : 'likes',
+                localField : '_id',
+                foreignField : 'tweet',
+                as : 'likeDetails',
+                pipeline : [
+                    {
+                        $project : {
+                            likedBy : 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields : {
+                likesCount : {
+                    $size : "$likeDetails"
+                },
+                ownerDetails : {
+                    $first: "$ownerDetails",
+                },
+                isLiked : {
+                    $in : [req.user?._id, "$likeDetails.likedBy"]
+                }
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                ownerDetails: 1,
+                likesCount: 1,
+                createdAt: 1,
+                isLiked: 1
+            },
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
