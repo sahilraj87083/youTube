17c221406af84199e22e673f43cb9f76c3770fd6
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-24 12:46:12 +0530

    feat(subscription): add toggleSubscription with self-check and validation

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,6 +1,6 @@
 import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
+import {User} from "../models/user.models.js"
+import { Subscription } from "../models/subscription.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -9,6 +9,53 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, 'Invalid ChannelId');
+    }
+
+    const channel = await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+    if (req.user._id.toString() === channelId.toString()) {
+        throw new ApiError(400, "You cannot subscribe to yourself")
+    }
+
+
+    const isSubscribed = await Subscription.findOne({
+        subscriber : req.user?._id,
+        channel : channelId
+    })
+
+    if(isSubscribed){
+        await Subscription.findByIdAndDelete(isSubscribed?._id)
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    { subscribed: false },
+                    "Unsubscribed successfully"
+                )
+            );
+    }
+
+    await Subscription.create({
+        subscriber : req.user?._id,
+        channel : channelId
+    })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { subscribed: true },
+                "subscribed successfully"
+            )
+        );
 })
 
 // controller to return subscriber list of a channel
