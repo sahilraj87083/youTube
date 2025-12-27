6ed6a2f79a36a40887946f92a7c90b8bf4b71b37
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-27 18:06:26 +0530

    Different routes configured

--- backend/src/routes/dashboard.routes.js
+++ backend/src/routes/dashboard.routes.js
@@ -11,3 +11,5 @@ router.use(verifyJWT) // Apply verifyJWT middleware to all routes in this file
 
 router.route('/stats').get(getChannelStats)
 router.route('/videos').get(getChannelVideos)
+
+export default router
